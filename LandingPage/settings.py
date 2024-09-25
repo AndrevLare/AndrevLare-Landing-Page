ecde5aac4332a6aedf499c6e58069851d51e8f91
Andrés Oviedo <jorgitoa0109@gmail.com>
2024-09-25 00:43:13 -0500

    :(
    Segundo intento

--- LandingPage/settings.py
+++ LandingPage/settings.py
@@ -121,15 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
-if not DEBUG: # Tell Django to copy statics to the staticfiles directory
-    # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # Turn on white Noise storage backend that takes care of compressing static files
-    # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storage. Compressed ManifestStaticFiles Storage'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
